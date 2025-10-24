10a777f51cda23cbd6b7c7379eed1da99c371931
Ashraf <ashrafkabbashi77@gmail.com>
2025-10-24 09:21:33 +0200

    Amelioration du style

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ function changeBackground() {
 }
 changeBackground()
 setInterval(changeBackground, 5000)
+
+window.onscroll = function() { scrollFunction() };
+
+
+function scrollFunction() {
+    const btn = document.getElementById("topBtn");
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        btn.style.display = "block";
+    }else {
+        btn.style.display = 'none';
+    }
+}
+function topFunction() {
+    window.scrollTo({top: 0, behavior: "smooth"});
+}
